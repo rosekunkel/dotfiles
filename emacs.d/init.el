53f201dd920c8215144f5cb94619ecd322cdacdc
Will Kunkel <will@wkunkel.com>
2017-05-23 22:28:35 -0400

    Renamed startup.org to true-init.org to avoid conflict with built-in file.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,23 +1,23 @@
 ;; We need this comment here or Emacs will add this line automatically.
 ;; (package-initialize)
 
-;; Load the real init file by extracting elisp code from startup.org and byte
+;; Load the real init file by extracting elisp code from true-init.org and byte
 ;; compiling it if it's changed since last time we started Emacs.
-(let* ((startup-org (locate-user-emacs-file "startup.org"))
-       (startup (file-name-sans-extension startup-org))
-       (startup-el (concat startup ".el"))
-       (startup-elc (concat startup ".elc")))
-  (when (or (file-newer-than-file-p startup-org startup-elc)
-            (file-newer-than-file-p user-init-file startup-elc))
+(let* ((true-init-org (locate-user-emacs-file "true-init.org"))
+       (true-init (file-name-sans-extension true-init-org))
+       (true-init-el (concat true-init ".el"))
+       (true-init-elc (concat true-init ".elc")))
+  (when (or (file-newer-than-file-p true-init-org true-init-elc)
+            (file-newer-than-file-p user-init-file true-init-elc))
     (require 'ob-tangle)
 
-    ;; Since our startup file is actually a symbolic link in the .emacs.d
+    ;; Since our true init file is actually a symbolic link in the .emacs.d
     ;; directory, we need to suppress a confirmation message when visiting the
     ;; file to tangle it.
     (let ((vc-follow-symlinks t))
-      (org-babel-tangle-file startup-org startup-el "emacs-lisp"))
+      (org-babel-tangle-file true-init-org true-init-el "emacs-lisp"))
 
-    (byte-compile-file startup-el)
+    (byte-compile-file true-init-el)
     (message "Byte-compiled")
-    (delete-file startup-el))
-  (load-file startup-elc))
+    (delete-file true-init-el))
+  (load-file true-init-elc))
